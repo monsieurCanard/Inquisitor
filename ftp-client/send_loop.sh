d7c12960c8b46581cffd52227264639319a24cf6
MonsieurC <mail.anthony42@gmail.com>
2026-01-17 12:47:14 +0100

    new: creation of ftp-client and server and script for exchange between them

---
+++ ftp-client/send_loop.sh
@@ -0,0 +1,6 @@
+while true; do
+	echo "coucou" > /tmp/file.txt &&
+	chmod 755 /tmp/file.txt &&
+	lftp -u inquisitor,pass7 ftp://ftp-server -e "set ftp:passive-mode on; put /tmp/file.txt; bye"
+	sleep 5
+done 
